eea17bce6ac4082d01bef6878bdfbc4f0f27ac90
SanjatulHasanSiam <sanjatulhasansiam.iit@gmail.com>
2023-11-02 14:49:28 +0600

    Navbar Created

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './navbar.css';
+import logo from '../../assets/logo.png';
+import contactImg from '../../assets/contact.png';
+import {Link} from 'react-scroll';
+const Navbar=()=>{
+    return(
+        <nav className="navbar">
+            <img src={logo} alt="Logo" className="logo"/>
+            <div className="desktopMenuBtn">
+                <Link className="desktopMenuListItem">Home</Link>
+                <Link className="desktopMenuListItem">About</Link>
+                <Link className="desktopMenuListItem">Portfolio</Link>
+                <Link className="desktopMenuListItem">Clients</Link>
+            </div>
+            <button className="desktoMenuBtn">
+                <img className="desktopMenuImg" src={contactImg} alt=""/>Contact Me
+            </button>
+        </nav>
+    )
+}
+export default Navbar;
